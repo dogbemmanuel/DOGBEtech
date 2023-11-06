269d3c4fcdb0bba924db1bb3eaf8c9a14a1cf90e
dogbemanuel <dogbemanuel@gmail.com>
2023-11-06 13:02:33 +0000

    Uploaded a cv

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './button.css'
-import CV from '../../assets/CV.docx';
+import EmmanuelDogb_Resume from '../../assets/CV.docx';
 
 
 const Button = () => {
   return (
     <div className='CTA'>
-        <a  href= {CV} download className='btn'>Download CV</a>
+        <a  href= {EmmanuelDogb_Resume} download className='btn'>Download CV</a>
         <a  href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
